8f5edf2fd19c33a1bbe1043fb55c7d5afd1e2407
leandro <leandro.vicente98@outlook.com>
2020-02-01 20:38:45 -0300

    Criado A Model De Anuncios

--- src/routes.js
+++ src/routes.js
@@ -2,12 +2,10 @@ const express = require("express");
 const routes = express.Router();
 
 const authMiddleware = require("./app/middlewares/auth");
+const controllers = require("./app/controllers");
 
-const UserController = require("./app/controllers/UserController");
-const SessionController = require("./app/controllers/SessionController");
-
-routes.post("/users", UserController.store);
-routes.post("/sessions", SessionController.store);
+routes.post("/users", controllers.UserController.store);
+routes.post("/sessions", controllers.SessionController.store);
 
 routes.get("/teste", authMiddleware, (req, res) => res.json({ ok: true }));
 
